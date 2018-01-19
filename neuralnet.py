f9b2e5a5ce9901f401de35e82bbac0195382f1fd
michaelplesser <34630761+michaelplesser@users.noreply.github.com>
2018-01-19 12:49:59 -0500

    removed unneeded import

--- neuralnet.py
+++ neuralnet.py
@@ -1,6 +1,5 @@
 import sys
 import numpy as np
-from sklearn.datasets        import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing   import StandardScaler
 from sklearn.neural_network  import MLPClassifier
